c6989b2f33aa33c8b60433c868c364d01d0bccdc
190ikp <red.volovo1996@outlook.com>
2019-03-20 07:35:49 +0900

    clean .gintignore

--- bash/cakey.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-cd /usr/local/etc
-mkdir ca
-chmod 600 ca
-cd ca
-openssl genrsa -out ca.key -aes256 2048
-
-openssl req -new -x509 -key -ca.key -aes256 -days 3650 -extensions v3_ca -out ca.pem
--- www/html/index.md
+++
@@ -1,6 +0,0 @@
-# Hello World!
-This is a test page.
-## Site List
-- [index page](http://www.example.com)
-- [Gitbucket](http://git.example.com)
-- [Redmine](http://task.example.com)
